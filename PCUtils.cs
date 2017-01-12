18f06db514c689d834502a82fc99145a3a72d399
Mirco <amirco@gmail.com>
2017-01-12 11:21:57 +0100

    aggiunto OSVersion fonte wxp

--- PCUtils.cs
+++ PCUtils.cs
@@ -10,6 +10,9 @@ namespace VS2010Commons
     {
         public static string CurrentUser = WindowsIdentity.GetCurrent().Name;
         public static string MachineName = Environment.MachineName;
+
+        public static string OSVersion = Environment.OSVersion.ToString();
+
         //public static string Name2 = System.Net.Dns.GetHostName();
         //public static string Name3 = System.Windows.Forms.SystemInformation.ComputerName;
         //public static string Name4 = System.Environment.GetEnvironmentVariable("COMPUTERNAME");
